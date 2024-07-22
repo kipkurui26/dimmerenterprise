ec521c67273111554489968902474a0500eb5c30
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-22 18:01:35 +0300

    Dimmer Enterprise LTD

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,18 +1,31 @@
-import TopNav from "./TopNav"
-import './navigation.css'
-import DimmerLogo from '../../assets/dimmer-logo.jpeg';
+/* NavBar.jsx */
+import TopNav from "./TopNav";
+import "./navigation.css";
+import DimmerLogo from "../../assets/dimmer-logo.jpeg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <section className="navigation">
-        <TopNav />
-        <nav className='nav'>
-            <figure className="nav__logo">
-                <img className="nav__logo--image" src={DimmerLogo} alt="Dimmer Enterprise LTD Logo" />
-            </figure>
-        </nav>
+      <TopNav />
+      <nav className="nav">
+        <figure className="nav__logo">
+          <Link to="/">
+            <img
+              className="nav__logo--image"
+              src={DimmerLogo}
+              alt="Dimmer Enterprise LTS Logo"
+            />
+          </Link>
+        </figure>
+        <ul className="nav__links"></ul>
+        <div className="nav__menu">
+          <GiHamburgerMenu className="nav__menu--button" />
+        </div>
+      </nav>
     </section>
-  )
-}
+  );
+};
 
 export default NavBar
