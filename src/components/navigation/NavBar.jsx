0b3484007da71784805b449c3d88d15f8d2ab25b
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-22 19:12:37 +0300

    Dimmer Enterprise LTD

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -4,8 +4,12 @@ import "./navigation.css";
 import DimmerLogo from "../../assets/dimmer-logo.jpeg";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import { navItems, subLinks } from "./NavList";
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <section className="navigation">
       <TopNav />
@@ -20,8 +24,37 @@ const NavBar = () => {
           </Link>
         </figure>
         <ul className="nav__links">
-            
+          {navItems.map(({ id, title, path, cName }) => (
+            <li key={id} className={cName}>
+              {title === "Services" ? (
+                <>
+                  <span
+                    className="nav__link--item"
+                    onClick={() => setShowLinks(!showLinks)}
+                  >
+                    {title} {showLinks ? <FaAngleUp /> : <FaAngleDown />}
+                  </span>
+                  {showLinks && (
+                    <ul className="nav__sub--links">
+                      {subLinks.map(({ id, title, path, cName }) => (
+                        <li key={id} className={cName}>
+                          <Link className="nav__link--item" to={path}>
+                            {title}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </>
+              ) : (
+                <Link className="nav__link--item" to={path}>
+                  {title}
+                </Link>
+              )}
+            </li>
+          ))}
         </ul>
+        <button className="nav__quote">Get A Quote</button>
         <div className="nav__menu">
           <GiHamburgerMenu className="nav__menu--button" />
         </div>
--- src/components/navigation/NavList.jsx
+++ src/components/navigation/NavList.jsx
@@ -3,24 +3,24 @@ export const navItems = [
     id: 1,
     title: "Home",
     path: "/",
-    cName: "nav-link",
+    cName: "nav__link",
   },
   {
     id: 2,
     title: "About",
     path: "/about-us",
-    cName: "nav-link",
+    cName: "nav__link",
   },
   {
     id: 3,
     title: "Services",
-    cName: "nav-link service-link",
+    cName: "nav__link service-link",
   },
   {
     id: 4,
     title: "Contact",
     path: "/contact-us",
-    cName: "nav-link",
+    cName: "nav__link",
   },
 ];
 
@@ -28,37 +28,37 @@ export const subLinks = [
     {
         id: 1,
         title: 'Hydro-Geological Survey',
-        path: '/hydro-geological-survey',
-        cName: "sub-link"
+        path: '/hydrogeological-survey',
+        cName: "nav__sub--link"
     },
     {
         id: 2,
         title: 'Borehole Drilling',
-        path: '/borehole-drilling-in-kenya',
-        cName: "sub-link"
+        path: '/borehole-drilling',
+        cName: "nav__sub--link"
     },
     {
         id: 3,
         title: 'Camera Inspection',
         path: '/camera-inspection',
-        cName: "sub-link"
+        cName: "nav__sub--link"
     },
     {
         id: 4,
         title: 'Test Pumping',
         path: '/test-pumping',
-        cName: "sub-link"
+        cName: "nav__sub--link"
     },
     {
         id: 5,
         title: 'Pump & Solar Installation',
         path: '/pump-and-solar-installation',
-        cName: "sub-link"
+        cName: "nav__sub--link"
     },
     {
         id: 6,
         title: 'Tower Construction',
         path: '/tower-construction',
-        cName: "sub-link"
+        cName: "nav__sub--link"
     },
 ]
