58aa69513fdd7e10532966c49ff9ca13a50f0e76
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-23 15:48:27 +0300

    Dimmer Enterprises LTD

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -41,30 +41,43 @@ const Footer = () => {
   ];
   return (
     <section className="footer">
-      <div className="footer__contanier footer__company">
-        <p className="footer__company--slogan">Your One Stop Solution.</p>
-        <h2 className="footer__company--title">Dimmer Enterpeises LTD</h2>
-        <p className="footer__company--description">
-          Dimmer Enterprises LTD is a prestigious borehole drilling company
-          based in Kenya, renowned for our commitment to excellence and
-          sustainability. With over 10 years of dedicated service to the
-          community, we have established ourselves as a trusted provider of
-          comprehensive water solutions.
-        </p>
-        <ul className="footer__company--icons">
-          {socialLinks.map(({ id, icon: IconComponent, link, target }) => (
-            <li className="footer__icons--list" key={id}>
-              <Link target={target} to={link} className="footer__icons--link">
-                <IconComponent />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="footer__contanier footer__services">
-        <h2 className="footer__title">Our services</h2>
-        <ul className="footer__services--list">
-          {subLinks.map(({ id, title, path }) => (
+      <div className="footer__cols">
+        <div className="footer__contanier footer__company">
+          <p className="footer__company--slogan">Your One Stop Solution.</p>
+          <h2 className="footer__company--title">Dimmer Enterpeises LTD</h2>
+          <p className="footer__company--description">
+            Dimmer Enterprises LTD is a prestigious borehole drilling company
+            based in Kenya, renowned for our commitment to excellence and
+            sustainability. With over 10 years of dedicated service to the
+            community, we have established ourselves as a trusted provider of
+            comprehensive water solutions.
+          </p>
+          <ul className="footer__company--icons">
+            {socialLinks.map(({ id, icon: IconComponent, link, target }) => (
+              <li className="footer__icons--list" key={id}>
+                <Link target={target} to={link} className="footer__icons--link">
+                  <IconComponent />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="footer__contanier footer__services">
+          <h2 className="footer__title">Our services</h2>
+          <ul className="footer__services--list">
+            {subLinks.map(({ id, title, path }) => (
+              <li key={id} className="footer__services--links">
+                <HashLink smooth to={path} className="footer__services--link">
+                  <FaChevronRight className="footer__services--icon" />
+                  {title}
+                </HashLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="footer__contanier footer__quicklinks">
+          <h2 className="footer__title">Usefull Links</h2>
+          {navItems.map(({ id, title, path }) => (
             <li key={id} className="footer__services--links">
               <HashLink smooth to={path} className="footer__services--link">
                 <FaChevronRight className="footer__services--icon" />
@@ -72,44 +85,36 @@ const Footer = () => {
               </HashLink>
             </li>
           ))}
-        </ul>
-      </div>
-      <div className="footer__contanier footer__quicklinks">
-        <h2 className="footer__title">Usefull Links</h2>
-        {navItems.map(({ id, title, path }) => (
-          <li key={id} className="footer__services--links">
-            <HashLink smooth to={path} className="footer__services--link">
-              <FaChevronRight className="footer__services--icon" />
-              {title}
-            </HashLink>
-          </li>
-        ))}
-      </div>
-      <div className="footer__contanier footer__contact">
-        <h2 className="footer__title">Reach Us</h2>
-        <div className="footer__contact--col">
-          <div className="footer__contact--content">
-            <span className="footer__content--title">Our Office</span>
-            <span className="footer__content--text">
-              Kabanas, Nairobi Kenya
-            </span>
-            <span className="footer__content--text">P.O. Box 3880-00506</span>
-          </div>
-          <div className="footer__contact--content">
-            <span className="footer__content--title">Call Us</span>
-            <span className="footer__content--text">0712 055 342</span>
-            <span className="footer__content--text">0780 055 342</span>
-          </div>
-          <div className="footer__contact--content">
-            <span className="footer__content--title">Email Us</span>
-            <span className="footer__content--text">
-              dimmerenterprisesltd@yahoo.com
-            </span>
+        </div>
+        <div className="footer__contanier footer__contact">
+          <h2 className="footer__title">Reach Us</h2>
+          <div className="footer__contact--col">
+            <div className="footer__contact--content">
+              <span className="footer__content--title">Our Office</span>
+              <span className="footer__content--text">
+                Kabanas, Nairobi Kenya
+              </span>
+              <span className="footer__content--text">P.O. Box 3880-00506</span>
+            </div>
+            <div className="footer__contact--content">
+              <span className="footer__content--title">Call Us</span>
+              <span className="footer__content--text">0712 055 342</span>
+              <span className="footer__content--text">0780 055 342</span>
+            </div>
+            <div className="footer__contact--content">
+              <span className="footer__content--title">Email Us</span>
+              <span className="footer__content--text">
+                dimmerenterprisesltd@yahoo.com
+              </span>
+            </div>
           </div>
         </div>
       </div>
       <div className="footer__copy">
-        <span className="footer__copyright">&copy; {new Date().getFullYear()} Dimmer Enterprises LTD | All Rights Reserved.</span>
+        <span className="footer__copyright">
+          &copy; {new Date().getFullYear()} Dimmer Enterprises LTD | All Rights
+          Reserved.
+        </span>
       </div>
     </section>
   );
