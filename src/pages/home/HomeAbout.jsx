e558a17fd1a746f5cb4b04e44c25999102b7e049
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-24 13:05:28 +0300

    Dimmer Enterprise LTD

--- src/pages/home/HomeAbout.jsx
+++ src/pages/home/HomeAbout.jsx
@@ -1,7 +1,12 @@
 const HomeAbout = () => {
   return (
     <section className="homeabout">
-        
+        <div className="homeabout__container">
+
+        </div>
+        <figure className="homeabout__figure">
+          
+        </figure>
     </section>
   )
 }
