f51a30c6970a30aa213b901aefdd9a44cb81f7a8
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-24 16:17:02 +0300

    Dimmer Enterprise LTD

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./components/navigation/NavBar";
 import Home from "./pages/home/Home";
 import Services from "./pages/services/Services";
 import Footer from "./components/footer/Footer";
+import About from "./pages/about/About";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path='/about-us' element={<About />} />
       </Routes>
       <Footer />
     </>
