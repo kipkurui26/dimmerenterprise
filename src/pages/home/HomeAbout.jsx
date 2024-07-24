8d7575f0b818e47970fba7847346aecb88c50cfd
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-24 13:16:27 +0300

    Dimmer Enterprise LTD

--- src/pages/home/HomeAbout.jsx
+++ src/pages/home/HomeAbout.jsx
@@ -1,14 +1,18 @@
 const HomeAbout = () => {
   return (
     <section className="homeabout">
-        <div className="homeabout__container">
-
-        </div>
-        <figure className="homeabout__figure">
-          
-        </figure>
+      <div className="homeabout__container">
+        <span className="homeabout__intro">We Are Dimmer Enterprises LTD.</span>
+        <h2 className="homeabout__title">Discover Who We Are</h2>
+        <p className="homeabout__description">
+          Dimmer Enterprises Limited is a leading provider of comprehensive borehole water solutions in Kenya. With over a decade of experience, we specialize in delivering high-quality services, including hydrogeological surveys, borehole drilling, pump installations, and sustainable water management systems. Our commitment to innovation, sustainability, and community development has made us a trusted partner for both residential and commercial projects across the region.
+        </p>
+      </div>
+      <figure className="homeabout__figure">
+        
+      </figure>
     </section>
-  )
-}
+  );
+};
 
-export default HomeAbout
+export default HomeAbout;
