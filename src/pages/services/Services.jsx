b4f3faec08c49be7d9b697c31db868d877f3b026
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-25 19:04:56 +0300

    Dimmer Enterprise LTD

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,23 +1,14 @@
 import './services.css'
-import { subLinks } from '../../components/navigation/NavList'
 import Survey from './Survey'
 import Drilling from './Drilling'
 import Camera from './Camera'
 import TestPumping from './TestPumping'
 import PumpSolar from './PumpSolar'
 import TowerConstruction from './TowerConstruction'
-import { HashLink } from 'react-router-hash-link'
 
 const Services = () => {
   return (
     <section className="services">
-        <ul>
-            {subLinks.map(({id, path, title})=>(
-                <li key={id}>
-                    <HashLink smooth to={path}>{title}</HashLink>
-                </li>
-            ))}
-        </ul>
         <Survey />
         <Drilling/>
         <Camera/>
