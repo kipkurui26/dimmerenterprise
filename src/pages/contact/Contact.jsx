a6c64348d245ad7947e9b699b55b009fbfcb693f
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-29 13:26:22 +0300

    Dimmer Enterprise Limited

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,18 @@
 import ContactForm from "../../components/contact_form/ContactForm";
+import MiniBanner from "../../components/minibanner/MiniBanner";
+import BannerImage from "../../assets/water.jpeg";
 import "./contact.css";
 
 const Contact = () => {
   return (
     <section className="contact">
+        <MiniBanner
+        isAbout={true}
+        miniBannerContent="We'd love to hear from you! Reach out to us using the form below or via the contact details provided."
+        breadcrumbTitle="Contact Us"
+        imageAlt="Contact Dimmer Enterprises LTD Banner"
+        imageImg={BannerImage}
+      />
       <ContactForm />
     </section>
   );
