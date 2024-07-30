34f9841ea46b59018367d6a1911e145a37fc6f52
kipkurui26 <6mutaibartai58@gmail.com>
2024-07-30 13:14:26 +0300

    Dimmer Enterprise Limited

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -6,7 +6,7 @@ import Camera from "./Camera";
 import TestPumping from "./TestPumping";
 import PumpSolar from "./PumpSolar";
 import TowerConstruction from "./TowerConstruction";
-import BannerImage from "../../assets/water.jpeg";
+import waterImageBanner from "../../assets/water.jpeg";
 import MiniBanner from "../../components/minibanner/MiniBanner";
 
 const Services = () => {
@@ -16,65 +16,83 @@ const Services = () => {
     {
       id: 1,
       breadcrumbTitle: "Hydrogeological Survey Services",
-      miniBannerContent: "Comprehensive hydrogeological surveys to identify groundwater resources and ensure optimal borehole placement.",
+      miniBannerContent:
+        "Comprehensive hydrogeological surveys to identify groundwater resources and ensure optimal borehole placement.",
       imageAlt: "Hydrogeological Survey Dimmer Enterprises LTD Banner",
+      hashLink: "/services#hydrogeological-survey",
     },
     {
       id: 2,
       breadcrumbTitle: "Borehole Drilling Services",
-      miniBannerContent: "Professional borehole drilling services for reliable water access in residential, commercial, and industrial applications.",
+      miniBannerContent:
+        "Professional borehole drilling services for reliable water access in residential, commercial, and industrial applications.",
       imageAlt: "Borehole Drilling Dimmer Enterprises LTD Banner",
+      hashLink: "/services#borehole-drilling",
     },
     {
       id: 3,
       breadcrumbTitle: "Borehole Camera Inspection Services",
-      miniBannerContent: "Advanced camera inspections to assess borehole conditions and ensure optimal performance and longevity.",
+      miniBannerContent:
+        "Advanced camera inspections to assess borehole conditions and ensure optimal performance and longevity.",
       imageAlt: "Borehole Camera Inspection Dimmer Enterprises LTD Banner",
+      hashLink: "/services#camera-inspection",
     },
     {
       id: 4,
       breadcrumbTitle: "Borehole Test Pumping Services",
-      miniBannerContent: "Thorough test pumping services to evaluate groundwater yield and quality, ensuring sustainable water management.",
+      miniBannerContent:
+        "Thorough test pumping services to evaluate groundwater yield and quality, ensuring sustainable water management.",
       imageAlt: "Borehole Test Pumping Dimmer Enterprises LTD Banner",
+      hashLink: "/services#test-pumping",
     },
     {
       id: 5,
       breadcrumbTitle: "Pump and Solar Installation Services",
-      miniBannerContent: "Efficient pump and solar installations for sustainable and cost-effective water supply solutions.",
+      miniBannerContent:
+        "Efficient pump and solar installations for sustainable and cost-effective water supply solutions.",
       imageAlt: "Pump and Solar Installation Dimmer Enterprises LTD Banner",
+      hashLink: "/services#pump-and-solar-installation",
     },
     {
       id: 6,
       breadcrumbTitle: "Elevated Tank Tower Construction Services",
-      miniBannerContent: "Robust tank and tower construction services for secure and reliable water storage solutions.",
-      imageAlt: "Elevated Tank Tower Construction Dimmer Enterprises LTD Banner",
+      miniBannerContent:
+        "Robust tank and tower construction services for secure and reliable water storage solutions.",
+      imageAlt:
+        "Elevated Tank Tower Construction Dimmer Enterprises LTD Banner",
+      hashLink: "/services#tower-construction",
     },
   ];
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentBanner((prevBanner) => (prevBanner + 1) % servicesList.length);
-    }, 8000); // 8 seconds
+    }, 8000);
 
-    return () => clearInterval(interval); // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, [servicesList.length]);
 
   return (
     <section className="services">
-      {servicesList.map((service, index) => (
-        <div
-          key={service.id}
-          style={{ display: currentBanner === index ? "block" : "none" }}
-        >
-          <MiniBanner
-            isAbout={false}
-            miniBannerContent={service.miniBannerContent}
-            breadcrumbTitle={service.breadcrumbTitle}
-            imageAlt={service.imageAlt}
-            imageImg={BannerImage}
-          />
-        </div>
-      ))}
+      <div className="services-banner-wrapper">
+        {servicesList.map((service, index) => (
+          <div
+            key={service.id}
+            className={`services-banner ${
+              currentBanner === index ? "visible" : ""
+            }`}
+          >
+            <MiniBanner
+              isAbout={false}
+              miniBannerContent={service.miniBannerContent}
+              breadcrumbTitle={service.breadcrumbTitle}
+              imageAlt={service.imageAlt}
+              imageImg={waterImageBanner}
+              hashLink={service.hashLink}
+            />
+          </div>
+        ))}
+      </div>
       <Survey />
       <Drilling />
       <Camera />
